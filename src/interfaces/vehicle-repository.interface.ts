13a905c33a7c546b172351a0bc2b9c5dc9f1697d
danteBenitez <benitezdante123@gmail.com>
2024-10-15 10:50:34 -0300

    fix: Permitir actualizaciones parciales de vehículos

--- src/interfaces/vehicle-repository.interface.ts
+++ src/interfaces/vehicle-repository.interface.ts
@@ -3,6 +3,6 @@ import { IVehicle } from "./vehicle.interface";
 export interface IVehicleRepository {
     create(client: Omit<IVehicle, "id">): Promise<IVehicle>;
     findById(id: string): Promise<IVehicle | null>;
-    update(client: IVehicle): Promise<IVehicle | null>;
+    update(client: Partial<IVehicle>): Promise<IVehicle | null>;
     delete(id: string): Promise<IVehicle | null>;
 }
