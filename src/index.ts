26f77f2071fa1ecde4ffaaabf24d713a0fc68dd3
danteBenitez <benitezdante123@gmail.com>
2024-10-15 15:36:01 -0300

    fix: Borrados archivos innecesarios

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from "./config/config.service.js";
-import { setupDatabase } from "./database/setup.js";
 import { Server } from "./server.js";
 
 const app = express();
@@ -8,11 +7,5 @@ const app = express();
 const server = new Server(app);
 
 const PORT = config.getServerPort();
-
-server
-    .onBeforeStart(async () => {
-        await setupDatabase()
-            .then(() => console.log(`Conexión exitosa a base de datos: ${config.getDatabaseOptions().DIALECT}`))
-    })
 
 server.start(PORT);
